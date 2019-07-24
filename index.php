b9dc782a2a1c36f1a59a05fd1ad732ff46ca9903
Tim Weber <tim.weber@v3consulting.com>
2019-07-24 18:08:44 +0200

    changed icons in footer

--- index.php
+++ index.php
@@ -246,9 +246,9 @@
 			Geöffnet ab 19:00 Uhr<br>
 			Nur für Bewohner der Studentenstadt Freimann
 			<div class="social-icons">
-				<a href="https://www.facebook.com/manhattanbarhsh/"><i class="fa fa-facebook-square"></i></a>
+				<a href="https://www.facebook.com/manhattanbarhsh/"><i class="fa fa-facebook-official"></i></a>
 				<a href="https://www.m.me/manhattanbarhsh"><i class="fa fa-facebook-messenger"></i></a>
-				<a href="mailto:manhattan@stusta.de"><i class="fa fa-envelope-square"></i></a>
+				<a href="mailto:manhattan@stusta.de"><i style="font-size:95%" class="fa fa-envelope"></i></a>
 				<a href="https://wiki.stusta.de/Manhattan"><i class="fa fa-book"></i></a>
 			</div>
 		</div>
