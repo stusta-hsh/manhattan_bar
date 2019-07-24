2f97424e45ae27c2fb023d01170ccd2e8fb078ef
Tim Weber <tim.weber@v3consulting.com>
2019-07-24 16:56:17 +0200

    added finances tab

---
+++ admin/finances_header.php
@@ -0,0 +1,14 @@
+<?php
+$page_title='team';
+
+?>
+<div class='toolbar-background'>
+	<div class='toolbar'>
+        <span style='color: #ccc'><i style='color: #ccc' class='fa fa-glass'></i><br>Produkte</span>
+		<span><a href='finances_ingredients.php'><i class='fa fa-flask'></i><br>Zutaten</a></span>
+		<span style='color: #ccc'><i style='color: #ccc' class='fa fa-list'></i><br>Inventur</span>
+		<span style='color: #ccc'><i style='color: #ccc' class='fa fa-trash'></i><br>Löschen</span>
+	</div>
+</div>
+</body>
+</html>
---
+++ admin/finances_ingredients.php
@@ -0,0 +1,35 @@
+<?php
+$page_title='finances';
+
+include('header.php');
+
+// Datenbankabfrage Liste aller Mitarbeiter
+$sql = 'SELECT ingredients.id, ingredients.name FROM ingredients';
+$sql_query = mysqli_prepare($db, $sql);
+if (!$sql_query) die('ERROR: could not prepare sql: $sql');
+mysqli_stmt_execute($sql_query);
+$ingredients = mysqli_stmt_get_result($sql_query);
+mysqli_stmt_close($sql_query);
+
+include('finances_header.php');
+
+?>
+	<div class="content">
+		<h3>Zutaten</h3>
+		<table class="ingredients_list">
+            <tr>
+                <th>ID</th>
+                <th>Zutat</th>
+                <th>Preis pro Einheit</th>
+            </tr>
+            <?php foreach($ingredients as $ingredient){ ?>
+				<tr>
+                    <td style="text-align: left"><?php echo($ingredient['id']);?></a></td>
+                    <td style="text-align: left"><?php echo($ingredient['name']);?></a></td>
+                    <td style="text-align: left"><?php  ?></a></td>
+				</tr>
+			<?php } ?>
+        </table>
+	</div>
+</body>
+</html>
