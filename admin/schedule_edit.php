b2fedd4893a22557289cebd830303cc3c9710a1b
Tim Weber <tim.weber@v3consulting.com>
2019-07-29 22:17:57 +0200

    fixed employee order in dropdowns

--- admin/schedule_edit.php
+++ admin/schedule_edit.php
@@ -38,7 +38,7 @@ $schedule_next = mysqli_fetch_assoc(mysqli_stmt_get_result($sql_query));
 mysqli_stmt_close($sql_query);
 
 // Datenbankabfrage Liste aller aktiven Mitarbeiter
-$sql = 'SELECT employees.id, employees.first_name, employees.last_name, employees.display_name, employees.room_number, employees.training_0, employees.training_1, employees.training_2, houses.name AS "house.name" FROM employees LEFT JOIN houses ON employees.house = houses.id  WHERE employees.deleted=0 AND employees.active=1 ORDER BY employees.first_name ASC, employees.last_name ASC';
+$sql = 'SELECT employees.id, employees.first_name, employees.last_name, employees.display_name, employees.room_number, employees.training_0, employees.training_1, employees.training_2, houses.name AS "house.name" FROM employees LEFT JOIN houses ON employees.house = houses.id  WHERE employees.deleted=0 AND employees.active=1 ORDER BY employees.display_name ASC, employees.last_name ASC';
 $sql_query = mysqli_prepare($db, $sql);
 if (!$sql_query) die('ERROR: could not prepare sql: $sql');
 mysqli_stmt_execute($sql_query);
