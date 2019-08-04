a01549ebbff4e7244867a559f0420f729c70088c
Tim Weber <tim.weber@v3consulting.com>
2019-08-04 16:36:13 +0200

    removed unused delete button

--- admin/schedule_list.php
+++ admin/schedule_list.php
@@ -21,7 +21,7 @@ mysqli_stmt_close($sql_query);
 			<span><a href='schedule_edit.php?id=<?php echo $schedule['id'] ?>'><i class='fa fa-calendar-o'></i><br>Aktueller Plan</a></span>
 			<span><a href='schedule_new.php'><i class='fa fa-calendar-plus-o'></i><br>Neuer Plan</a></span>
 			<span style='color: #ccc'><i style='color: #ccc' class='fa fa-print'></i><br>Drucken</span>
-			<span style='color: #ccc'><i style='color: #ccc' class='fa fa-trash'></i><br>Löschen</span>
+			<!--<span style='color: #ccc'><i style='color: #ccc' class='fa fa-trash'></i><br>Löschen</span>-->
 		</div>
 		<div class='toolbar'>
 			<span><a href="schedule_list.php?y=<?php echo($month==1?$year-1:$year) ?>&m=<?php echo($month==1?'12':$month-1) ?>"><i class='fa fa-chevron-left'></i><br></a></span>
