5c18da13e807810d5eb86b63c243c53a1dc8b3b4
Tim Weber <tim.weber@v3consulting.com>
2019-11-14 18:31:53 +0100

    wip printable for the new poster design

---
+++ admin/schedule_print_new.php
@@ -0,0 +1,156 @@
+<?php
+
+$rota = parse_ini_file('../wochenplan_'.date('W').'.ini');
+$weekdays = ['so', 'mo', 'di', 'mi', 'do', 'fr', 'sa'];
+$months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+$days_from_monday = [6, 0, 1, 2, 3, 4, 5];
+if(isset($_GET['id'])) $id = $_GET['id'];
+
+$servername = "localhost";
+$username = "e00038";
+$password = "hfDAJSDWy7vR5Pmd";
+$dbname = "e00038a";
+
+$db = mysqli_connect($servername, $username, $password, $dbname);
+if(!$db) exit("Database connection error: ".mysqli_connect_error());
+
+$page_title='schedules';
+
+require('fpdf181/fpdf.php');
+
+// Datenbankabfrage Wochenplan
+$sql = 'SELECT * FROM schedules WHERE id= ?';
+$sql_query = mysqli_prepare($db, $sql);
+mysqli_stmt_bind_param($sql_query, 'i', $id);
+if (!$sql_query) die('ERROR: could not prepare sql: $sql');
+mysqli_stmt_execute($sql_query);
+$schedule = mysqli_fetch_assoc(mysqli_stmt_get_result($sql_query));
+mysqli_stmt_close($sql_query);
+
+if(empty($schedule)){
+	header('Location: schedule_list.php');
+	exit();
+}
+
+// Datenbankabfrage Liste aller aktiven Mitarbeiter
+$sql = 'SELECT employees.id, employees.first_name, employees.last_name, employees.display_name, employees.room_number, houses.name AS "house.name" FROM employees LEFT JOIN houses ON employees.house = houses.id  WHERE employees.deleted=0 AND employees.active=1 ORDER BY employees.first_name ASC, employees.last_name ASC';
+$sql_query = mysqli_prepare($db, $sql);
+if (!$sql_query) die('ERROR: could not prepare sql: $sql');
+mysqli_stmt_execute($sql_query);
+$employees = mysqli_stmt_get_result($sql_query);
+mysqli_stmt_close($sql_query);
+
+$employee_names=[];
+foreach($employees as $employee){
+	if(!empty($employee['display_name']))
+		$employee_names[$employee['id']]=$employee['display_name'];
+	else
+		$employee_names[$employee['id']]=$employee['first_name'];
+}
+
+//PDF-variables
+$borders = 1; //draw cell-borders 0, 1, R, T, L, B
+$line_height = 6;
+$line_margin = 8;
+
+$pdf = new FPDF();
+$pdf->AddFont('raleway','','Raleway-Medium.php');
+
+$pdf->SetTitle('Wochenplan '.$schedule['year'].'-'.$schedule['calendar_week']);
+$pdf->SetAuthor(ucfirst($_SERVER['PHP_AUTH_USER']));
+$pdf->SetCreator('Manhattan WebApp');
+
+$pdf->AddPage();
+$pdf->SetMargins(20, 10);
+$pdf->SetFont('Raleway', '', 15);
+//$pdf->Image('../images/logo.png', 23, 10, -300, -300);
+$pdf->SetY(70);
+$pdf->SetFontSize(20);
+/*if(date('n', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-1')) == date('n', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-7'))){
+	$pdf->Cell(0, $line_height, date('j.', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-1')).' - '.date('j. ', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-7')).$months[date('n', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-7'))-1], 0, 0, 'C');
+}else{
+	$pdf->Cell(0, $line_height, date('j. ', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-1')).$months[date('n', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-1'))-1].' - '.date('j. ', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-7')).$months[date('n', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-7'))-1], 0, 0, 'C');
+}*/
+$pdf->SetFontSize(16);
+$pdf->SetY(90);
+// Für jeden Wochentag
+for($i=4; $i<7; $i++){
+	$x=$pdf->GetX();
+	$y=$pdf->GetY();
+	// Hintergrundfarbe weiß
+	$pdf->SetDrawColor(256, 256, 256);
+	$pdf->SetFillColor(256, 256, 256);
+	// Padding oben in der Zeile
+	$pdf->Cell(0, $line_margin, '', $borders, 1, 'C', 'true');
+
+	// Wochentagkürzel
+	$pdf->Cell(15, $line_height, ucfirst($weekdays[$i%7]), $borders, 2, 'C', 'true');
+	// Datum TT.MM.
+	$pdf->SetFontSize(12);
+	$pdf->Cell(15, $line_height, date('j.n.', strtotime($schedule['year'].'-W'.$schedule['calendar_week'].'-'.$i)), $borders, 0, 'C', 'true');
+	$pdf->SetFontSize(16);
+	$pdf->SetXY($x+15, $y+$line_margin);
+
+	// Tagestext
+	if(!$schedule[$weekdays[$i%7].'_open']){
+		// geschlossen
+		$pdf->SetTextColor(100, 100, 100);
+		$pdf->Cell(100, $line_height*2, 'geschlossen', $borders, 0, 'C', 'true');
+		$pdf->SetTextColor(0, 0, 0);
+	}elseif(!$schedule[$weekdays[$i%7].'_deal'] && !$schedule[$weekdays[$i%7].'_event']){
+		// Geöffnet, aber weder Deal noch Event eingetragen
+		$pdf->Cell(100, $line_height*2, iconv('UTF-8', 'windows-1252', 'Geöffnet'), $borders, 0, 'C', 'true');
+	}elseif(!$schedule[$weekdays[$i%7].'_event']){
+		// Geöffnet, kein Event eingetragen
+		if(strpos($schedule[$weekdays[$i%7].'_deal'], '<br>')){
+			$pdf->MultiCell(100, $line_height, iconv('UTF-8', 'windows-1252', str_replace("<br>","\n",$schedule[$weekdays[$i%7].'_deal'])), $borders, 'C', 'true');
+		}else{
+			$pdf->Cell(100, $line_height*2, iconv('UTF-8', 'windows-1252', str_replace("<br>","",$schedule[$weekdays[$i%7].'_deal'])), $borders, 0, 'C', 'true');
+		}
+	}elseif(!$schedule[$weekdays[$i%7].'_deal']){
+		// Geöffnet, kein Deal eingetragen
+		$pdf->Cell(100, $line_height*2, iconv('UTF-8', 'windows-1252', str_replace("<br>","",$schedule[$weekdays[$i%7].'_event'])), $borders, 0, 'C', 'true');
+	}else{
+		// Geöffnet, Deal und Event eingetragen
+		$pdf->Cell(100, $line_height, iconv('UTF-8', 'windows-1252', $schedule[$weekdays[$i%7].'_event']), $borders, 2, 'C', 'true');
+		$pdf->SetFontSize(12);
+		$pdf->Cell(100, $line_height, iconv('UTF-8', 'windows-1252', str_replace("<br>","",str_replace("<sup>","",str_replace("</sup>","",$schedule[$weekdays[$i%7].'_deal'])))), $borders, 0, 'C', 'true');
+		$pdf->SetFontSize(16);
+	}
+
+	$pdf->SetXY($x+115, $y+$line_margin);
+
+	// Team
+	if(!$schedule[$weekdays[$i%7].'_open']){
+		// geschlossen
+		$pdf->Cell(0, $line_height*2, '', $borders, 1, 'C', 'true');
+	}elseif(!$schedule[$weekdays[$i%7].'_kueche']){
+		// Geöffnet, keine Küchenschicht eingetragen
+		if($schedule[$weekdays[$i%7].'_springer']){
+			$pdf->Cell(0, $line_height*2, iconv('UTF-8', 'windows-1252', $employee_names[$schedule[$weekdays[$i%7].'_theke']].' & '.$employee_names[$schedule[$weekdays[$i%7].'_springer']]), $borders, 1, 'C', 'true');
+		}else{
+			$pdf->Cell(0, $line_height*2, iconv('UTF-8', 'windows-1252', $employee_names[$schedule[$weekdays[$i%7].'_theke']]), $borders, 1, 'C', 'true');
+		}
+	}else{
+		// Geöffnet, Küchenschicht eingetragen
+		if(!$schedule[$weekdays[$i%7].'_springer']){
+			$pdf->Cell(0, $line_height*2, iconv('UTF-8', 'windows-1252', $employee_names[$schedule[$weekdays[$i%7].'_kueche']].' & '.$employee_names[$schedule[$weekdays[$i%7].'_theke']]), $borders, 2, 'C', 'true');
+		}else{
+			$pdf->Cell(0, $line_height, iconv('UTF-8', 'windows-1252', $employee_names[$schedule[$weekdays[$i%7].'_kueche']].','), $borders, 2, 'C', 'true');
+			$pdf->Cell(0, $line_height, iconv('UTF-8', 'windows-1252', $employee_names[$schedule[$weekdays[$i%7].'_theke']].' & '.$employee_names[$schedule[$weekdays[$i%7].'_springer']]), $borders, 1, 'C', 'true');
+		}
+	}
+	$pdf->Cell(0, $line_margin, '', $borders, 1, 'C', 'true');
+}
+$pdf->SetY($y+30);
+//$pdf->SetDrawColor(0, 0, 0);
+//$pdf->SetFillColor(0, 0, 0);
+//$pdf->Rect(0,280,250,100,'DF');
+//$pdf->Image('../images/skyline_static.png', 10, 246, 190);
+//$pdf->SetFontSize(13);
+//$pdf->Cell(0, 8, iconv('UTF-8', 'windows-1252', 'HSH 21. Stock | Geöffnet ab 19 Uhr'), 0, 2, 'C');
+//$pdf->Cell(0, 8, iconv('UTF-8', 'windows-1252', 'Wochenplan und aktueller Status unter manhattan.stusta.de'), 0, 0, 'C');
+
+$pdf->Output('I', 'wochenplan_'.$schedule['year'].'-'.$schedule['calendar_week']);
+
+?>
