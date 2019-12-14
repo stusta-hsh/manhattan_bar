a5362ca352ffdc9acc4f2ab881138d7614f0b613
Tim Weber <tim.weber@v3consulting.com>
2019-12-14 20:57:54 +0100

    wip adding website settings

--- admin/settings.php
+++ admin/settings.php
@@ -35,7 +35,48 @@ if($_POST){
 			<div class="card-title">Webseite</div>
 			<div class="card-content">
 				<form method='post' action=''>
-					<textarea rows="4" name="footer_text"><?php echo $settings['footer_text'] ?></textarea>
+					<label class="flex-100">Design
+						<select name="stylesheet_id">
+							<option value='0' <?php if($settings['stylesheet_id']==0)echo'selected' ?>>Standard</option>
+							<option value='1' <?php if($settings['stylesheet_id']==1)echo'selected' ?>>Winter</option>
+						</select>
+					</label>
+					<label class="flex-100">Urlaubstext
+						<textarea rows="4" name="away_text"><?php echo $settings['away_text'] ?></textarea>
+					</label>
+					<label class="flex-100">Text im Footer
+						<textarea rows="4" name="footer_text"><?php echo $settings['footer_text'] ?></textarea>
+					</label>
+					<label class="flex-100">
+						<input name="facebook_icon" type="hidden" value="0">
+						<input name="facebook_icon" type="checkbox" value="1" <?php if($settings['facebook_icon']==1)echo'checked' ?>>
+						Facebook Icon
+						<input type="text" name="facebook_url" value="<?php echo $settings['facebook_url'] ?>">
+					</label>
+					<label class="flex-100">
+						<input name="messenger_icon" type="hidden" value="0">
+						<input name="messenger_icon" type="checkbox" value="1" <?php if($settings['messenger_icon']==1)echo'checked' ?>>
+						Messenger Icon
+						<input type="text" name="messenger_url" value="<?php echo $settings['messenger_url'] ?>">
+					</label>
+					<label class="flex-100">
+						<input name="instagram_icon" type="hidden" value="0">
+						<input name="instagram_icon" type="checkbox" value="1" <?php if($settings['instagram_icon']==1)echo'checked' ?>>
+						Instagram Icon
+						<input type="text" name="instagram_url" value="<?php echo $settings['instagram_url'] ?>">
+					</label>
+					<label class="flex-100">
+						<input name="email_icon" type="hidden" value="0">
+						<input name="email_icon" type="checkbox" value="1" <?php if($settings['email_icon']==1)echo'checked' ?>>
+						E-Mail Icon
+						<input type="text" name="email_url" value="<?php echo $settings['email_url'] ?>">
+					</label>
+					<label class="flex-100">
+						<input name="wiki_icon" type="hidden" value="0">
+						<input name="wiki_icon" type="checkbox" value="1" <?php if($settings['wiki_icon']==1)echo'checked' ?>>
+						Wiki Icon
+						<input type="text" name="wiki_url" value="<?php echo $settings['wiki_url'] ?>">
+					</label>
 					<br>
 					<input type='submit' value='Anwenden'></input>
 				</form>
