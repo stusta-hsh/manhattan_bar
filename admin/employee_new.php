6b0f4a878b702d5c19042549628fcc1efe4bf398
Tim Weber <tim.weber@stusta.de>
2020-04-11 22:30:56 +0200

    applied universal form style to employee creator

--- admin/employee_new.php
+++ admin/employee_new.php
@@ -27,94 +27,100 @@ if($_POST){
 include('employee_header.php');
 
 ?>
-	<div class='content'>
-		<div class='edit_employee_form'>
-			<form method='post' action=''>
-				<h3>Neuer Mitarbeiter</h3>
-				<div class="employee-card">
-					<label>Vorname
-						<input name="first_name" type="text">
-					</label>
-					<label>Nachname
-						<input name="last_name" type="text">
-					</label>
-					<label title="Wenn leer, wird der Vorname angezeigt">Spitzname (Anzeigename)
-						<input name="display_name" type="text">
-					</label>
-
-					<br>
-					<label>Haus
-						<select name="house">
-							<?php foreach($houses as $house){ ?>
-								<option value='<?php echo $house['id'] ?>' <?php if($house['name']=='HSH')echo'selected' ?>><?php echo $house['name']; if(!empty($house['alias']))echo(' ('.$house['alias'].')'); ?></option>
-							<?php } ?>
-						</select>
-					</label>
-					<label>Zimmer
-						<input name="room_number" type="number">
-					</label>
-					<label>Geburtstag
-						<input name="birthday" type="date">
-					</label>
-
-					<br>
-					<label>E-Mail
-						<input name="email" type="email">
-					</label>
-					<label>Handy
-						<input name="phone" type="fon">
-					</label>
-
-				</div>
-				<div class="employee-card">
-					<label>Aktiv
-						<input name="active" type="hidden" value="0">
-						<input name="active" type="checkbox" value="1" checked>
-					</label>
-					<label>
-						<input type="radio" name="role"  value="0" checked>Mitarbeiter
-						<input type="radio" name="role"  value="1">Betreiber
-						<input type="radio" name="role"  value="2">Ausschuss
-					</label>
-
-					<br>
-					<label>Im Team seit
-						<input name="date_employed" type="date" value="<?php echo(date('Y-m-d')); ?>">
-					</label>
-
-					<br>
-					<label>Einarbeitung Theke
-						<input name="training_0" type="hidden" value="0">
-						<input name="training_0" type="checkbox" value="1">
-					</label>
-					<label>Einarbeitung Dachterrasse
-						<input name="training_1" type="hidden" value="0">
-						<input name="training_1" type="checkbox" value="1">
-					</label>
-					<label>Einarbeitung Küche
-						<input name="training_2" type="hidden" value="0">
-						<input name="training_2" type="checkbox" value="1">
-					</label>
-					<label>Hygienebelehrung
-						<input name="health_certificate" type="hidden" value="0">
-						<input name="health_certificate" type="checkbox" value="1">
-					</label>
-					<label>Einkäufer
-						<input name="buyer" type="hidden" value="0">
-						<input name="buyer" type="checkbox" value="1">
-					</label>
-					<label>Putzkraft
-						<input name="cleaner" type="hidden" value="0">
-						<input name="cleaner" type="checkbox" value="1">
-					</label>
-
-					<br>
-					<label>Kommentar
-						<textarea rows="4" name="comment"><?php echo $employee['comment'] ?></textarea>
-					</label>
-				</div>
-				<input type='submit' value='Speichern'>
-			</form>
+	<div class="content">
+		<div class="card">
+			<div class="card-title">
+				Neuer Mitarbeiter
+			</div>
+			<div class="card-content">
+				<form method='post' action=''>
+					<div class="card-form-box">
+						<div class="card-form-row">
+							<label class="flex-100">Vorname
+								<input name="first_name" type="text">
+							</label>
+							<label class="flex-100">Nachname
+								<input name="last_name" type="text">
+							</label>
+							<label class="flex-100" title="Wenn leer, wird der Vorname angezeigt">Spitzname (Anzeigename)
+								<input name="display_name" type="text">
+							</label>
+						</div>
+						<div class="card-form-row">
+							<label class="flex-200">Haus
+								<select name="house">
+									<?php foreach($houses as $house){ ?>
+										<option value='<?php echo $house['id'] ?>' <?php if($house['name']=='HSH')echo'selected' ?>><?php echo $house['name']; if(!empty($house['alias']))echo(' ('.$house['alias'].')'); ?></option>
+									<?php } ?>
+								</select>
+							</label>
+							<label class="flex-100">Zimmer
+								<input name="room_number" type="number">
+							</label>
+							<label class="flex-100">Geburtstag
+								<input name="birthday" type="date">
+							</label>
+						</div>
+						<div class="card-form-row">
+							<label class="flex-100">E-Mail
+								<input name="email" type="email">
+							</label>
+							<label class="flex-100">Handy
+								<input name="phone" type="tel">
+							</label>
+						</div>
+					</div>
+					<div class="card-form-box">
+						<div class="card-form-row">
+							<input name="active" type="hidden" value="1">
+							<label class="flex-100">Position
+								<select name="role">
+									<option value="0">Mitarbeiter</option>
+									<option value="1">Betreiber</option>
+									<option value="2">Ausschussmitglied</option>
+								</select>
+							</label>
+							<label class="flex-100">Im Team seit
+								<input name="date_employed" type="date" value="<?php echo(date('Y-m-d')); ?>">
+							</label>
+						</div>
+						<div class="card-form-row">
+							<label>Einarbeitung Theke
+								<input name="training_0" type="hidden" value="0">
+								<input name="training_0" type="checkbox" value="1">
+							</label>
+							<label>Einarbeitung Dachterrasse
+								<input name="training_1" type="hidden" value="0">
+								<input name="training_1" type="checkbox" value="1">
+							</label>
+							<label>Einarbeitung Küche
+								<input name="training_2" type="hidden" value="0">
+								<input name="training_2" type="checkbox" value="1">
+							</label>
+						</div>
+						<div class="card-form-row">
+							<label>Hygienebelehrung
+								<input name="health_certificate" type="hidden" value="0">
+								<input name="health_certificate" type="checkbox" value="1">
+							</label>
+							<label>Einkäufer
+								<input name="buyer" type="hidden" value="0">
+								<input name="buyer" type="checkbox" value="1">
+							</label>
+							<label>Putzkraft
+								<input name="cleaner" type="hidden" value="0">
+								<input name="cleaner" type="checkbox" value="1">
+							</label>
+						</div>
+						<div class="card-form-row">
+							<label class="flex-300">Kommentar
+								<textarea rows="4" name="comment"><?php echo $employee['comment'] ?></textarea>
+							</label>
+						</div>
+					</div>
+					<input type='submit' value='Speichern'>
+				</form>
+			</div>
 		</div>
 	</div>
 </body>
