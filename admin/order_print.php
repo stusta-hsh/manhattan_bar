1dcb6b156c8b03c07f729dd72dd1ad0fe7ad26b9
Tim Weber <tim.weber@stusta.de>
2020-04-15 18:33:02 +0200

    further improvements on order printout

--- admin/order_print.php
+++ admin/order_print.php
@@ -11,40 +11,38 @@
 	$db = mysqli_connect($sql_host, $sql_username, $sql_password, $sql_dbname);
 	if(!$db) exit("Database connection error: ".mysqli_connect_error());
 
-	$orders = mysqli_query($db, "SELECT o.id, o.date, o.name, o.phone, o.paid, o.comment, h.name as house, o.room FROM orders o JOIN houses h ON (o.house = h.id) ORDER BY -o.paid, -o.house");
+	$orders = mysqli_query($db, "SELECT o.id, o.date, o.name, o.phone, o.paid, o.comment, h.name as house, o.room FROM orders o JOIN houses h ON (o.house = h.id) ORDER BY -o.paid, o.house, o.room");
 
 	foreach ($orders as $order) { ?>
 
 		<p> --------------------------------------------------------------------------- </p>
-		<p> ## Bestellung <?php echo $order['id']; ?> (Eingegangen <?php echo $order['date']; ?>) ## </p>
-		<p> Besteller: <?php echo $order['name']; ?> <br/> Haus: <?php echo $order['house']; ?>, Zimmer <?php echo $order['room']; ?> </p>
+		<p> ## Bestellung <?php echo $order['id']; ?> (<?php echo $order['date']; ?>) ## </p>
+		<p> <?php echo $order['name']; ?> (<?php echo $order['house']; ?>, <?php echo $order['room']; ?>) </br> Telefon: <?php echo $order['phone']; ?> </p>
 		<?php if(!$order['paid']) echo '<p> NICHT BEZAHLT </p>'; ?>
 
 		<ul>
 			<?php $positions = mysqli_query($db, "SELECT * FROM menu_positions WHERE order_id = $order[id]");
 			foreach ($positions as $position) { ?>
+
+				<?php
+					switch ($position['patty']) {
+						case 0:
+							$position['cheese'] ? $burger = 'Cheeseburger' : $burger = 'Hamburger';
+							break;
+						case 1:
+							$burger = 'Beyond-Meat-Burger';
+							if($position['cheese']) $burger .= ' mit Käse';
+							break;
+						case 2:
+							$burger = 'Double-Burger';
+							if($position['cheese']) $burger .= ' mit Käse';
+							break;
+					}
+				?>
+
 				<li>
-				 Bestellung:
+				 <strong><?php echo $burger ?></strong>
 					<ul>
-						<?php if($position['patty'] != 0) {
-							$patty = '';
-							switch ($position['patty']) {
-								case 1:
-									$patty = 'Beyond-Meat';
-									break;
-								case 2:
-									$patty = 'Double-Burger';
-									break;
-								default:
-									'Burger';
-							}
-							if($position['cheese'] != 0) {
-								$cheese = 'Cheese-';
-							} else {
-								$cheese = ' ';
-							}
-								echo "<li> <strong> $cheese $patty </strong></li>";
-						} ?>
 						<?php if(!$position['salad']) { ?> <li> OHNE Salat </li> <?php } ?>
 						<?php if(!$position['tomato']) { ?> <li> OHNE Tomate </li> <?php } ?>
 						<?php if(!$position['onion']) { ?> <li> OHNE Zwiebel </li> <?php } ?>
@@ -52,7 +50,7 @@
 						<?php if($position['friedonions']) { ?> <li> MIT Röstzwiebeln </li> <?php } ?>
 						<?php if($position['pickles']) { ?> <li> MIT Essiggurken </li> <?php } ?>
 						<?php if($position['bacon']) { ?> <li> Extra: Bacon </li> <?php } ?>
-						<?php if($position['camembert']) { ?> <li> Extra: Cammenbert </li> <?php } ?>
+						<?php if($position['camembert']) { ?> <li> Extra: Camembert </li> <?php } ?>
 						<?php if($position['beilage'] == 1) { ?> <li> Beilage: Pommes </li> <?php } ?>
 						<?php if($position['beilage'] == 2) { ?> <li> Beilage: Wedges </li> <?php } ?>
 						<?php if($position['bier'] != 0) {
@@ -75,8 +73,6 @@
 				</li>
 			<?php } ?>
 		</ul>
-
-		<p> ** <?php echo $order['comment']; ?> ** </p>
-
+		<?php if($order['comment'] != '') echo '<p>** '.$order['comment'].' **</p>'; ?>
 	<?php } ?>
 </body>
