0855cfaf92813b9224fe4839a7aa841e34b2f2b3
Tim Weber <tim.weber@stusta.de>
2020-05-28 17:05:21 +0200

    add printable list to navigation

--- admin/order_header.php
+++ admin/order_header.php
@@ -20,7 +20,8 @@ $date = (isset($_GET['date']) ? $_GET['date'] : date('Y-m-d'));
 <div class='toolbar-background'>
 	<div class='toolbar'>
 		<span><a href='order_list.php'><i class='fa fa-list'></i><br>Alle Bestellungen</a></span>
-		<span><a href='order_print.php'><i class='fa fa-print'></i><br>Liste</a></span>
+		<span><a href='order_print.php'><i class='fa fa-print'></i><br>Liste alt</a></span>
+		<span><a href='order_print_list.php'><i class='fa fa-print'></i><br>Liste neu</a></span>
 		<span><a href="<?php echo('order_print_stickers.php?date='.$date) ?>"><i class='fa fa-print'></i><br>Etiketten</a></span>
 	</div>
 </div>
