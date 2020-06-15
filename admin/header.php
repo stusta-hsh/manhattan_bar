1d928efb0ad20169d145233570d75664827684a0
Tim Weber <tim.weber@stusta.de>
2020-06-16 01:25:51 +0200

    check year when fetching current schedule

--- admin/header.php
+++ admin/header.php
@@ -12,10 +12,10 @@
 	if(!$db) exit("Database connection error: ".mysqli_connect_error());
 
 	// Datenbankabfrage aktueller Wochenplan
-	$sql = 'SELECT * FROM schedules WHERE calendar_week = ?';
+	$sql = 'SELECT * FROM schedules WHERE calendar_week = ? AND year = ?';
 	$sql_query = mysqli_prepare($db, $sql);
-	if (!$sql_query) die('ERROR: could not prepare sql: '.$sql);
-	mysqli_stmt_bind_param($sql_query, 'i', date('W'));
+	if (!$sql_query) die('ERROR: Failed to prepare SQL.');
+	mysqli_stmt_bind_param($sql_query, 'is', date('W'), date('Y'));
 	mysqli_stmt_execute($sql_query);
 	$current_schedule = mysqli_fetch_assoc(mysqli_stmt_get_result($sql_query));
 	mysqli_stmt_close($sql_query);
